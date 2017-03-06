1939c8e2944adc87feb9de4f7e858417f6937963
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2017-03-06 17:21:40 +0100

    Produces working executables
    Creates the simpact cyan executables and adjust environment variables
    to that the RSimpactCyan package can find them.

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,18 @@
-.onLoad <- function( libname, pkgname )
+.onLoad <- function(libname, pkgname)
 {
-	print("onLoad")
+    baseDir <- system.file(package=pkgname)
+    exeDir <- file.path(baseDir, "exec")
+    pythonDir <- file.path(baseDir, "simpact","python")
+    dataDir <- paste(file.path(baseDir, "simpact","data"), .Platform$file.sep, sep="")
+
+    #print(baseDir)
+    print(paste("Using executable directory:", exeDir))
+    print(paste("Setting PYTHONPATH to:", pythonDir))
+    print(paste("Setting data directory to:", dataDir))
+
+    Sys.setenv(PATH=paste(exeDir, Sys.getenv("PATH"),sep=.Platform$path.sep))
+    Sys.setenv(PYTHONPATH=pythonDir)
+    Sys.setenv(SIMPACT_DATA_DIR=dataDir)
 }
 
 .onUnload <- function( libpath )
