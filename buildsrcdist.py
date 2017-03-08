eccd9c56a9e7e1cc6def4f60452c5023be0730d8
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2017-03-08 16:01:51 +0100

    Added build scripts, moved package itself

---
+++ buildsrcdist.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import subprocess
+import platform
+import tempfile
+import tarfile
+import shutil
+import sys
+import os
+
+def getRCommand():
+
+    if platform.system() == "Windows": # Windows
+        curDrive = os.getcwd()[:2]
+        for n in os.listdir(curDrive + "\\Program Files\\R\\"):
+            print n
+            if n.startswith("R-3"):
+                fn = curDrive + "\\Program Files\\R\\" + n + "\\bin\\R.exe"
+                if os.path.exists(fn):
+                    return fn
+        
+        raise Exception("Unable to find R executable")
+       
+    return "R"
+
+def createSourcePackage(Rcmd = "R"):
+    
+    tmpDir = tempfile.mkdtemp()
+    print "Created dir", tmpDir
+
+    curDir = os.getcwd()
+    print "Current dir", curDir
+
+    tarBall = subprocess.check_output([ "git", "archive", "--format", "tar", "--prefix=archive/", "HEAD"])
+
+    try:
+        os.chdir(tmpDir)
+        with open("archive.tar", "wb") as f:
+            f.write(tarBall)
+
+        tar = tarfile.open("archive.tar", "r")
+        tar.extractall()
+        tar.close()
+
+        subprocess.check_call([ Rcmd, "CMD", "build", os.path.join("archive","pkg")])
+        sourcePackage = [ n for n in os.listdir(".") if n.endswith(".tar.gz")][0]
+    finally:
+        os.chdir(curDir)
+
+    return (tmpDir, sourcePackage)
+
+if __name__ == "__main__":
+
+    d,p = createSourcePackage(getRCommand())
+
+    if os.path.exists(p):
+        os.unlink(p)
+
+    shutil.move(os.path.join(d,p), ".")
+    shutil.rmtree(d)
+
